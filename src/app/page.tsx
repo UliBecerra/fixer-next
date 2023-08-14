a4e414ce57692414c846c29cdbcd12314707f0af
uli <aldoulisesbc@gmail.com>
2023-08-14 16:24:40 -0600

    first deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import Image from 'next/image'
-
+'use client'
+import { useEffect } from 'react'
+import axios from 'axios'
 export default function Home() {
+  useEffect(() => {
+    const URL = 'http://data.fixer.io/api/latest?access_key=0e1a407873ed6ebb4a6c0ac05daec89b'  
+    
+    axios
+    .get(URL)
+    .then((res) => {
+      console.log(res.data) ;
+    })
+    .catch((err) => console.log(err));
+  
+    return () => {
+      
+    }
+  }, [])
+
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <h1>Divisas </h1>
+      <h2>Tarifas y monedas</h2>
+      
+    </main>
+  )
+}
+
+
+/* 
+
+<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
@@ -108,6 +135,4 @@ export default function Home() {
           </p>
         </a>
       </div>
-    </main>
-  )
-}
+*/
